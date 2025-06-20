5c831f5d7685b3dcf926932feb0f0e16fb147e1a
NathanCerne <90366735+NathanCerne@users.noreply.github.com>
2025-06-20 11:48:06 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+//javascript ping pong server
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+server.listen(3000, '0.0.0.0', () => {
+    console.log('Server running at http://0.0.0.0:3000/');
+});
+
+//console.log('Server running and listening on port 3000');
+
+io.on('connection', socket => {
+    console.log('Client connected', socket.id);
+
+    //listens for ping events
+    socket.on('ping', (data) => {
+        console.log(`Ping received from ${socket.id} with timestamp: ${data.timestamp}`);
+        //respond with pong
+        socket.emit('pong', { timestamp: data.timestamp });
+    });
+});
